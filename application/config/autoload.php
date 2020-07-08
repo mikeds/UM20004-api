054ba03afd62651fdff22fe09979651171e591a1
Marknel Pogi <marknel.pineda23@gmail.com>
2020-07-08 09:53:18 +0800

    Added: QR code - client transaction

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'utility', 'form', 'text', 'security', 'email');
+$autoload['helper'] = array('url', 'utility', 'form', 'text', 'security', 'email', 'qr_helper');
 
 /*
 | -------------------------------------------------------------------

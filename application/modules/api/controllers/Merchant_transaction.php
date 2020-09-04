3cdef293cd2ff82d843ae7a19e28288e129fe375
Marknel Pogi <marknel.pineda23@gmail.com>
2020-09-04 23:00:11 +0800

    Added: Merchant History

--- application/modules/api/controllers/Merchant_transaction.php
+++ application/modules/api/controllers/Merchant_transaction.php
@@ -16,6 +16,119 @@ class Merchant_transaction extends Merchant_Controller {
 		$this->load->model('api/wallets_model', 'wallets');
 	}
 
+
+	public function history($transaction_id = "") {
+		header('Content-type: application/json');
+		$post = json_decode($this->input->raw_input_stream, true);
+		$message = "";
+
+		$limit = isset($post["limit"]) ? $post["limit"] : $this->_limit;
+
+		$wallet_address = $this->_merchant['wallet_address'];
+
+		// default filter
+		$where = array(
+			'transaction_requested_by' => $wallet_address
+		);
+
+		if ($transaction_id != "") {
+			// checking if transaction id is belong to wallet address
+			$check_row = $this->transactions->get_datum(
+				'',
+				array(
+					'transaction_requested_by' => $wallet_address,
+					'transaction_id' => $transaction_id
+				)
+			)->row();
+
+			if ($check_row == "") {
+				$message = array(
+					'error' => true,
+					'error_descriptionn' => "Invalid transaction id!"
+				);
+
+				goto end;
+			}
+		
+			$where = array(
+				'transaction_requested_by' => $wallet_address,
+				'transaction_id <' => $transaction_id
+			);
+		}
+
+		$total_rows = $this->transactions->get_count(
+			$where
+		);
+		
+		// $offset = $this->get_pagination_offset($page, $limit, $total_rows);
+		$offset = 0;
+
+		$data = $this->transactions->get_data(
+			array("*"),
+			$where,
+			array(),
+			array(),
+			array(
+				'filter' => "transaction_date_created",
+				'sort' => "DESC"
+			),
+			$offset, // offset
+			$limit // limit
+		);
+
+		$data_1 = $this->transactions->get_data(
+			array("*"),
+			$where,
+			array(),
+			array(),
+			array(
+				'filter' => "transaction_date_created",
+				'sort' => "ASC"
+			),
+			0, // offset
+			1 // limit
+		);
+
+		$last_data = isset($data_1[0]) ? $data_1[0] : NULL;
+		$last_transaction_id = is_null($last_data) ? NULL : floatval($last_data['transaction_id']);
+		
+		$results = array();
+
+		foreach ($data as $datum) {
+			$type = get_transaction_type($datum['transaction_type_id']);
+			$status = get_transaction_status($datum['transaction_status']);
+
+			$amount = $datum['transaction_amount'];
+			$amount = floatval($amount);
+
+			$transaction_number = $datum['transaction_number'];
+
+			$results[] = array(
+				'transaction_id'	=> floatval($datum['transaction_id']),
+				'transaction_number' => strtoupper($transaction_number),
+				'transactionn_qr_code'	=> base_url() . "transaction/qr-code-{$transaction_number}",
+				'status' 			=> $status,
+				'type'				=> $type,
+				'amount'			=> $amount,
+				'date'				=> $datum['transaction_date_created'],
+				'date_expiration'	=> $datum['transaction_date_expiration'],
+			);
+		}
+
+		$message = array(
+			'value' => 
+			array(
+				'last_transaction_id'	=> $last_transaction_id,
+				'history'	=> $results
+			)
+		);
+
+		end:
+		echo json_encode($message);
+		http_response_code(200);
+		die();
+	}
+
 	public function accept_transaction() {
 		header('Content-type: application/json');
 		$post = json_decode($this->input->raw_input_stream, true);

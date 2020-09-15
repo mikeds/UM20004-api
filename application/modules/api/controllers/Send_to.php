2338a28ebe6c2ae40226e0976db97589d540a003
Marknel Pogi <marknel.pineda23@gmail.com>
2020-09-16 01:23:53 +0800

    Removed: send-to OTP

--- application/modules/api/controllers/Send_to.php
+++ application/modules/api/controllers/Send_to.php
@@ -79,13 +79,60 @@ class Send_to extends Client_Controller {
         $sender_ref_id  = $tx_row['sender_ref_id'];
         $pin            = $tx_row['pin'];
 
-        $email_address = $account->account_email_address;
+        // create ledger
+        $debit_amount	= $amount;
+        $credit_amount 	= $amount;
+        $fee_amount		= $fee;
 
-        $this->send_otp_pin(
-            "BambuPAY Transfer OTP PIN",
-            $email_address, 
-            $pin
+        $debit_total_amount 	= 0 - $debit_amount; // make it negative
+        $credit_total_amount	= $credit_amount;
+
+        $debit_wallet_address		= $this->get_wallet_address($debit_oauth_bridge_id);
+        $credit_wallet_address	    = $this->get_wallet_address($credit_oauth_bridge_id);
+        
+        if ($credit_wallet_address == "" || $debit_wallet_address == "") {
+            die();
+        }
+
+        $debit_new_balances = $this->update_wallet($debit_wallet_address, $debit_total_amount);
+        if ($debit_new_balances) {
+            // record to ledger
+            $this->new_ledger_datum(
+                "cash_in_debit", 
+                $transaction_id, 
+                $credit_wallet_address, // request from credit wallet
+                $debit_wallet_address, // requested to debit wallet
+                $debit_new_balances
+            );
+        }
+
+        $credit_new_balances = $this->update_wallet($credit_wallet_address, $credit_total_amount);
+        if ($credit_new_balances) {
+            // record to ledger
+            $this->new_ledger_datum(
+                "cash_in_credit", 
+                $transaction_id, 
+                $debit_wallet_address, // debit from wallet address
+                $credit_wallet_address, // credit to wallet address
+                $credit_new_balances
+            );
+        }
+
+        $this->transactions->update(
+            $transaction_id,
+            array(
+                'transaction_status' 		=> 1,
+                'transaction_date_approved' => $this->_today
+            )
         );
+
+        // $email_address = $account->account_email_address;
+
+        // $this->send_otp_pin(
+        //     "BambuPAY Transfer OTP PIN",
+        //     $email_address, 
+        //     $pin
+        // );
         
         echo json_encode(
             array(

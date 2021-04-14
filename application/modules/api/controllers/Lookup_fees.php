c4b3303345cfe387c84d0dd5ed17c189c30a42f2
Marknel Pogi <marknel.pineda23@gmail.com>
2021-04-14 09:09:14 +0800

    Fixed: lookup fees

--- application/modules/api/controllers/Lookup_fees.php
+++ application/modules/api/controllers/Lookup_fees.php
@@ -12,9 +12,9 @@ class Lookup_fees extends Api_Controller {
 			goto end;
 		}
 
-		$post       = $this->get_post();
-		$amount		= $post['amount'];
-		$tx_type_id = $post['tx_type_id'];
+		$post       			= $this->get_post();
+		$amount					= $post['amount'];
+		$transaction_type_id 	= $post['tx_type_id'];
 
 		if ($tx_type_id == "") {
 			echo json_encode(
@@ -27,19 +27,20 @@ class Lookup_fees extends Api_Controller {
 			die();
 		}
 
-		$fees = $this->get_fee(
-			$amount,
-			$tx_type_id
-		);
+        // get fee
+        $fee = $this->get_fee(
+            $amount,
+            $transaction_type_id
+        );
 
-		$total_amount = $amount + $fees;
+		$total_amount = $amount + $fee;
 
 		echo json_encode(
 			array(
 				'message' 	=> "Succesfully retrieve tx fee! ",
 				'response'	=> array(
 					'amount'		=> $amount,
-					'fees'			=> $fees,
+					'fees'			=> $fee,
 					'total_amount'	=> $total_amount
 				),
 				'timestamp'	=> $this->_today
